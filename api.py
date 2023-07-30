9ac4acd93c716af55d48dc1232b07d814f2ebf2d
物理伴你成长 <dean_qwq@outlook.com>
2023-07-30 19:23:10 +0800

    Add files via upload

---
+++ api.py
@@ -0,0 +1,25 @@
+﻿import json
+from selenium import webdriver
+import requests
+import re
+import random
+
+#requests.post('http://localhost:8000/')
+def keyword(message, uid, gid= None):
+    if message[0:3] == 'tq': # 天气
+        return tianqi(uid, gid, message[3:len(message)])
+
+def tianqi(uid, gid, name):
+
+    url = 'https://www.msn.cn/zh-cn/weather/forecast/in-' + name
+    driver = webdriver.Edge()
+    driver.maximize_window()
+    driver.get(url)
+    driver.save_screenshot('/data/images/result.png')
+    if gid != None: # 群聊信息
+        requests.get(url='http://127.0.0.1:1981/send_group_msg?group_id={0}&message={1}'.format(gid, r'[CQ:image,file=result.png]'))
+        return 'OK'
+
+    else: # 私聊信息
+        requests.get(url='http://127.0.0.1:1981/send_private_msg?user_id={0}&message={1}'.format(uid, name, r'[CQ:image,file=result.png]'))
+        return 'OK'
---
+++ wlbot.py
@@ -0,0 +1,25 @@
+﻿from flask import Flask, request
+import requests
+
+import api
+
+app = Flask(__name__)
+
+@app.route('/', methods=["POST"])
+def post_data():
+    '下面的request.get_json().get......是用来获取关键字的值用的，关键字参考上面代码段的数据格式'
+    if request.get_json().get('message_type') == 'private':  # 是私聊信息
+        uid = request.get_json().get('sender').get('user_id')  # 获取信息发送者的QQ号
+        message = request.get_json().get('raw_message')  # 获取原始信息
+        api.keyword(message, uid)  # 传到后台
+
+    if request.get_json().get('message_type') == 'group':  # 是群聊信息
+        gid = request.get_json().get('group_id')  # 获取群号
+        uid = request.get_json().get('sender').get('user_id')  # 获取信息发送者的QQ号
+        message = request.get_json().get('raw_message')  # 获取原始信息
+        api.keyword(message, uid, gid)  # 传到后台
+
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=11045)
